4f15b544a22aa99f8e4e979ff5effd1f66f1dd4b
ghopkins3 <ghopkins3@murraystate.edu>
2025-01-30 17:59:22 -0600

    Added exercise table and function to add exercise button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 import { getFromDB, insertToDB } from "./db.js";
 
 const dateDisplay = document.querySelector("#date-display");
+const addExerciseLink = document.querySelector(".add-exercise");
+const strengthContainer = document.querySelector(".strength-container");
+const exerciseFormTemplate = document.querySelector("#exercise-form-template")
+const exerciseForm = exerciseFormTemplate.content.cloneNode(true);
+
+
 
 let currentDate = new Date().toJSON().slice(0, 10);
+
+addExerciseLink.addEventListener("click", () => {
+    strengthContainer.appendChild(exerciseForm);
+});
 
 dateDisplay.value = currentDate;
 
