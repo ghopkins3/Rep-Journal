9d44e3cfedcb0101681608a4071bb9573d6ffe81
ghopkins3 <ghopkins3@murraystate.edu>
2025-01-30 18:44:03 -0600

    Changes

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ import { getFromDB, insertToDB } from "./db.js";
 const dateDisplay = document.querySelector("#date-display");
 const addExerciseLink = document.querySelector(".add-exercise");
 const strengthContainer = document.querySelector(".strength-container");
+
 const exerciseFormTemplate = document.querySelector("#exercise-form-template")
 const exerciseForm = exerciseFormTemplate.content.cloneNode(true);
-
-
 
 let currentDate = new Date().toJSON().slice(0, 10);
 
@@ -15,4 +14,3 @@ addExerciseLink.addEventListener("click", () => {
 });
 
 dateDisplay.value = currentDate;
-
