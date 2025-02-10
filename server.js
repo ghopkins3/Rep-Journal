4165de8c475ee8ef55d846a18f91dfe0a74ea7a8
ghopkins3 <ghopkins3@murraystate.edu>
2025-02-10 17:54:06 -0600

    Implemented testing APIs and tests

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import env from "dotenv";
-import {createClient} from "@supabase/supabase-js";
+import {createClient, RealtimeChannel} from "@supabase/supabase-js";
 env.config();
 const supabase = createClient(process.env.DATABSE_URL, process.env.DATABASE_SECRET_KEY);
 const app = express();
 let PORT = process.env.PORT;
+app.use(express.json());
 
 app.get("/test", async (req, res) => {
     try {
@@ -16,7 +17,7 @@ app.get("/test", async (req, res) => {
     }
 });
 
-app.get("/test/:date", async (req, res) => {
+app.get("/test/date=:date", async (req, res) => {
     try {
         const {data, error} = await supabase
             .from("test")
@@ -29,8 +30,71 @@ app.get("/test/:date", async (req, res) => {
     }
 });
 
+app.get("/test/id=:id", async (req, res) => {
+    try {
+        const {data, error} = await supabase
+            .from("test")
+            .select()
+            .eq("test_id", req.params.id);
+        console.log(data);
+        return res.send(data);
+    } catch (error) {
+        return res.send({error});
+    }
+});
+
+app.post("/test", async (req, res) => {
+    const { error } = await supabase
+        .from("test")
+        .insert({
+            test: req.body.test,
+            date: req.body.date,
+            test_id: req.body.test_id
+        });
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+    res.status(201).send("created!!");
+}); 
+
+app.delete("/test/date=:date", async (req, res) => {
+    const {error} = await supabase
+        .from("test")
+        .delete()
+        .eq("date", req.params.date)
+    if(error) {
+        return res.status(400).json({error: error.message});
+    }
+    res.status(201).send("deleted items created on date: " + req.params.date);
+});
+
+app.delete("/test/id=:id", async (req, res) => {
+    const {error} = await supabase
+        .from("test")
+        .delete()
+        .eq("test_id", req.params.id)
+    if(error) {
+        return res.status(400).json({error: error.message});
+    }
+    res.status(201).send("deleted item with id: " + req.params.id);
+});
+
+app.put("/test/id=:id", async (req, res) => {
+    const {error} = await supabase
+        .from("test")
+        .update({
+            test: req.body.test
+        })
+        .eq("test_id", req.params.id)
+    if(error) {
+        return res.status(400).json({error: error.message});
+    }
+    res.status(201).send("updated item with id: " + req.params.id);
+});
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello Worldington!");
 });
 
 app.listen(PORT, () => {
