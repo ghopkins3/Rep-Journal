56ca56c2dc1cf5ec5907c6662cbb7d26d6f18bf8
ghopkins3 <ghopkins3@murraystate.edu>
2025-03-27 21:42:12 -0500

    test

--- api/index.js
+++ api/index.js
@@ -1,23 +1,8 @@
-import express from "express";
-import cors  from "cors";
-import { supabase } from "../lib/backendSupabaseClient.js";
-import { convertToDatabaseFormat } from "../utils/formatUtils.js";
-import { postSignUp } from "../auth/postSignUp.js";
-import { postLogin } from "../auth/postLogin.js";
-import { supabaseAuthMiddleware } from "../auth/supabaseAuthMiddleware.js";
-import { getUser } from "../auth/getUser.js";
-import { putUser } from "../auth/putUser.js";
-import { deleteUser } from "../auth/deleteUser.js";
-
+const express = require("express");
 const app = express();
-let PORT = process.env.PORT;
-app.use(express.json());
-app.use(cors());
 
-app.use("/", (req, res) => {
-    res.send("Express on Vercel.");
-})
-
-app.listen(PORT, () => {
-    console.log(new Date().toLocaleTimeString() + `: Server is running on port ${PORT}...`)
+app.use("/",  (req, res) => {
+    res.send("server is running, better go catch it.");
 });
+
+app.listen(3000, console.log("Server started on PORT 3000"));
