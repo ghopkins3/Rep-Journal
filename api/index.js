f5a2809fa9d1345f00981366f38ba8a4a43a13a6
ghopkins3 <ghopkins3@murraystate.edu>
2025-03-27 19:13:43 -0500

    Add folders

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
+import express from "express";
+import cors  from "cors";
+import { supabase } from "../lib/backendSupabaseClient.js";
+import { convertToDatabaseFormat } from "../utils/formatUtils.js";
+import { postSignUp } from "../auth/postSignUp.js";
+import { postLogin } from "../auth/postLogin.js";
+import { supabaseAuthMiddleware } from "../auth/supabaseAuthMiddleware.js";
+import { getUser } from "../auth/getUser.js";
+import { putUser } from "../auth/putUser.js";
+import { deleteUser } from "../auth/deleteUser.js";
 
-import { supabase } from "./lib/backendSupabaseClient.js";
-import { convertToDatabaseFormat } from "./utils/formatUtils.js";
-import { postSignUp } from "./auth/postSignUp.js";
-import { postLogin } from "./auth/postLogin.js";
-import { supabaseAuthMiddleware } from "./auth/supabaseAuthMiddleware.js";
-import { getUser } from "./auth/getUser.js";
-import { putUser } from "./auth/putUser.js";
-import { deleteUser } from "./auth/deleteUser.js";
-
-const cors = require("cors");
-const express = require("express");
 const app = express();
 let PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/exercise/id=:id", supabaseAuthMiddleware, async (req, res) => {
     try {
@@ -348,10 +349,8 @@ app.get("/auth/user", supabaseAuthMiddleware, getUser);
 app.put("/auth/user", supabaseAuthMiddleware, putUser);
 app.delete("/auth/user", supabaseAuthMiddleware, deleteUser);
 
-app.get("/", async (req, res) => {
-    return res.send("hello world!");
-});
-
 app.listen(PORT, () => {
     console.log(new Date().toLocaleTimeString() + `: Server is running on port ${PORT}...`)
 });
+
+export default app;
