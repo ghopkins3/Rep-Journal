176c7be4adb6570a6c1ef8d629806758d9f3787b
ghopkins3 <ghopkins3@murraystate.edu>
2025-04-19 20:50:11 -0500

    updates

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ passwordDigitReq.classList.add("invalid-req");
 passwordSymbolReq.classList.add("invalid-req");
 
 let test = testing;
-let t = t;
+
 
 
 let hiddenItemCount = JSON.parse(localStorage.getItem("hiddenItemCount")) || [];
