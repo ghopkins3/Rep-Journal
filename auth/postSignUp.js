44f9f82ab96afd14db5c4a1b708e3a1563462e20
ghopkins3 <ghopkins3@murraystate.edu>
2025-04-21 20:50:26 -0500

    Added check for unique email/usernames when signing up

--- auth/postSignUp.js
+++ auth/postSignUp.js
@@ -1,17 +1,47 @@
 import { createAuthUser } from "./createAuthUser.js";
 import { createDbUser } from "./createDbUser.js";
+import { supabase } from "../lib/backendSupabaseClient.js";
 
 export const postSignUp = async (req, res) => {
     let {email, password, username} = req.body;
-    console.log(req.body);
 
-    try {
-        let authID = await createAuthUser(email, password);
+    username = username.trim();
+    email = email.trim();
 
-        await createDbUser({ auth_id: authID, username, email })
+    const { count: usernameCount, error: usernameError } = await supabase
+        .from("user")
+        .select("username", { count: "exact", head: true })
+        .ilike("username", username);
+    console.log("data:", usernameCount);
+    console.log("username:", username);
 
-        res.status(200).send("Successfully signed user up");
-    } catch(error) {
-        res.status(400).send("Failed to sign user up");
+
+    const { count: emailCount, error: emailError } = await supabase
+        .from("user")
+        .select("username", { count: "exact", head: true })
+        .ilike("username", email);
+    console.log("data:", emailCount);
+    console.log("username:", email);
+
+    if(usernameError) {
+        res.status(500).send(error.message, "Username already exists.");
     }
+
+    if(emailError) {
+        res.status(500).send(error.message, "Email address already exists.")
+    }
+
+    // if(usernameCount === 1) {
+    //     res.status(409).send({ error: "Username already taken." });
+    // } else {
+    //     try {
+    //         let authID = await createAuthUser(email, password);
+    
+    //         await createDbUser({ auth_id: authID, username, email })
+    
+    //         res.status(200).send("Successfully signed user up");
+    //     } catch(error) {
+    //         res.status(400).send("Failed to sign user up");
+    //     }
+    // }
 };
