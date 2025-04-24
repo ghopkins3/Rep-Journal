3cf9859d49dabb8f4fc287d200b14f33e5d276d8
ghopkins3 <ghopkins3@murraystate.edu>
2025-04-23 20:59:16 -0500

    updates

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ const submitSignUpBtn = document.querySelector(".submit-sign-up-button");
 const collapseOrExpandBtn = document.querySelector(".collapse-expand-button");
 collapseOrExpandBtn.classList.add("hidden");
 
-let test = "test";
-
 const signupEmailInput = signUpDialog.querySelector(".email-input");
 const signupUsernameInput = signUpDialog.querySelector(".username-input");
 const signupPasswordInput = signUpDialog.querySelector(".password-input");
